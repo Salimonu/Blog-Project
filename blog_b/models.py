4275a4573192d4889b89b73063059869084533fd
Hammed <salimonuh@gmail.com>
2025-05-19 08:36:43 +0100

    May_19_Modified

--- blog_b/models.py
+++ blog_b/models.py
@@ -1,13 +1,19 @@
+from django import forms
 from django.db import models
 
-from wagtail.models import Page, Orderable
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.fields import RichTextField
+from wagtail.models import Page, Orderable
 from wagtail.search import index
+from wagtail.snippets.models import register_snippet
 
-from modelcluster.fields import ParentalKey
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 class BlogIndexPage(Page):
   intro = RichTextField(blank=True)
+
   def get_context(self, request):
     context = super().get_context(request)
     blogpages = self.get_children().live().order_by('-first_published_at')
@@ -16,20 +22,68 @@ class BlogIndexPage(Page):
   
   content_panels = Page.content_panels + ["intro"]
 
+class BlogTagIndexPage(Page):
+
+  def get_context(self, request):
+    tag = request.GET.get('tag')
+    blogpages = BlogPage.objects.filter(tags__name=tag)
+    
+    context = super().get_context(request)
+    context['blogpages'] = blogpages
+    return context
+
+class BlogPageTag(TaggedItemBase):
+  content_object = ParentalKey(
+    'BlogPage',
+    related_name='tagged_items',
+    on_delete=models.CASCADE
+  )
+
 class BlogPage(Page):
   date = models.DateField("Post date")
   intro = models.CharField(max_length=250)  
   body = RichTextField(blank=True)
+  authors = ParentalManyToManyField('blog_b.Author', blank=True)
+  tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+  def main_image(self):
+    gallery_item = self.gallery_images.first()
+    if gallery_item:
+      return gallery_item.image
+    else:
+      return None
 
   search_fields = Page.search_fields + [
     index.SearchField('intro'),
     index.SearchField('body'),
   ]
 
-  content_panels = Page.content_panels + ["date", "intro", "body", "gallery_images",]
+  content_panels = Page.content_panels + [
+    MultiFieldPanel(children=["date",
+                     FieldPanel("authors", widget=forms.CheckboxSelectMultiple), "tags",], heading="Blog information"),
+    "intro", "body", "gallery_images"]
+
 
 class BlogPageGalleryImage(Orderable):
   page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
   image = models.ForeignKey(
-    'wagtailimages.Images', 
+    'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
   )
+  caption = models.CharField(blank=True, max_length=250)
+  panels = ["image", "caption"]
+
+@register_snippet
+class Author(models.Model):
+  name = models.CharField(max_length=255)
+  author_image = models.ForeignKey(
+    'wagtailimages.Image', null=True, blank=True,
+    on_delete=models.SET_NULL, related_name='+'
+  )
+  
+  panels = ["name", "author_image",]
+
+  def __str__(self):
+    return self.name
+  
+  class Meta:
+    verbose_name_plural = 'Authors'
--- home/models.py
+++ home/models.py
@@ -1,16 +1,47 @@
 from django.db import models
 
-from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.fields import RichTextField
+from wagtail.models import Page
+
 
 
 class HomePage(Page):
+    image = models.ForeignKey("wagtailimages.Image",
+                              null=True, blank=True, on_delete=models.SET_NULL,
+                              related_name="+", help_text="Homepage Image",
+                              )
+    hero_text = models.CharField(
+        blank=True, max_length=255, help_text="Write an introduction for the site."
+    )
+    hero_cta = models.CharField(
+        blank=True, verbose_name="Hero CTA", max_length=255,
+        help_text="Text to display on call to Action",
+    )
+    hero_cta_link = models.ForeignKey(
+        "wagtailcore.Page", 
+        null=True, blank=True, on_delete=models.SET_NULL,
+        related_name="+", verbose_name="Hero CTA Link",
+        help_text="Choose a page to link to for the Call to Action.",
+    )
+
     body = RichTextField(blank=True)
+
     def get_context(self, request):
         context = super().get_context(request)
         blogpages = self.get_children().live().order_by('-first_published_at')
         context['blogpages'] = blogpages
         return context
 
-    content_panels = Page.content_panels + ["body"]
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("image"),
+                FieldPanel("hero_text"),
+                FieldPanel("hero_cta"),
+                FieldPanel("hero_cta_link"),
+            ], heading = "Hero section",
+        ),
+        FieldPanel("body"),
+    ]
     
